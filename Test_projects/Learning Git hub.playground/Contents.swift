2f069fb09ba6925dfae1f3b7318a2ae43a013b17
paulsUsername <32492218+paulsUsername@users.noreply.github.com>
2017-10-03 18:16:50 +0100

    added my new playground project

---
+++ Test_projects/Learning Git hub.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var str2 = "Hello, world. I hate you"
+
+
+let a1 = 43
+
+let a2 = 44
+
+print(a1 + a2)
+
+
